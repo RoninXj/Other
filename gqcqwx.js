34f7fccd9816f5ed17456fb28c9c5eaa80fe72ee
RoninXj <48503899+RoninXj@users.noreply.github.com>
2022-12-21 15:51:53 +0800

    Delete gqcqwx.js

--- gqcqwx.js
+++
@@ -1,7 +0,0 @@
-$prefs.setValueForKey($request.headers.token, 'mytoken');
-
-if (typeof $prefs.valueForKey('mytoken') !== "undefined"){
-    $notify('传淇小程序', mytoken 获取成功,数据已存储!, $request['headers']['token']);
-}
-
-$done();
